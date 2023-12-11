fcb92b9e030a0437b6d6f494f25ef8be51e7e710
Đạt Tấn <nguyentandat01102002@gmail.com>
2023-12-11 09:40:51 +0700

    change method controller paymentcontroller

--- src/webshopping/mvc/controllers/PaymentController.php
+++ src/webshopping/mvc/controllers/PaymentController.php
@@ -1,8 +1,65 @@
 <?php
     class PaymentController extends Controller{
+        private $categories;
+        private $products;
+
+        public function __construct()
+        {   
+
+            // load product
+            $model = $this->model("Product");
+            $data_product = $model->LoadProducts();
+
+            // load category
+            $model = $this->model("Category");
+            $data_category = $model->LoadCategories();
+
+            foreach($data_category as $each){
+                $key = $this->Str2Url($each->getName());
+                $parent_name = $this->Str2Url($each->getParent_category_name());
+                $each->setParent_category_name($parent_name);
+                $data[$key] = $each;
+            }
+
+            $this->products = $data_product;
+            $this->categories = $data;
+        }
+
         function Show(){
             $data = [];
+                        // chuyển data về dạng key value để dễ for
+            $tmp = [];
+            foreach($this->categories as $key => $value){
+                $tmp[$value->getParent_category_name()][$key] =  $value->getName();
+            }
+
+            $data["categories"] = $tmp;
+            
             $page = $this->view("payment", $data);
+        }
+
+        function Delivery(){
+            $data = [];
+            if(isset($_SESSION['usr']) && isset($_SESSION['usr']['cart_code'])){
+                $tmp['cart_code'] = $_SESSION['usr']['cart_code'];
+                $model = $this->model("CartItem");
+                //var_dump($model);
+                $data['cartItem'] = $model->LoadCartItem($tmp);
+                //var_dump($data['cartItem']);
+            }   
+            else{
+                header("Location: /Auth");
+            }
+            
+            // chuyển data về dạng key value để dễ for
+            $tmp = [];
+            foreach($this->categories as $key => $value){
+                $tmp[$value->getParent_category_name()][$key] =  $value->getName();
+            }
+
+            $data["categories"] = $tmp;
+            
+            $page = $this->view("delivery", $data);
         }
     }
 ?>
