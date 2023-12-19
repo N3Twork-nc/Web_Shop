d706cc0182cf5882b424a830a2cffbb85ad094ac
Đạt Tấn <nguyentandat01102002@gmail.com>
2023-12-19 22:33:02 +0700

    phan quyen customer

--- src/webshopping/mvc/models/CategoryModel/Category.php
+++ src/webshopping/mvc/models/CategoryModel/Category.php
@@ -23,50 +23,5 @@ include_once "./mvc/models/CategoryModel/CategoryObj.php";
                     return  $sql . "<br>" . $e->getMessage();
                 }
         }
-
-        function InsertCategory($data){
-            try {
-                $db = new DB();
-                $sql = "INSERT INTO `Categories`(`name`, `parent_category_id`) VALUES (?, ?);";
-                $params = array($data['category_name'], $data['category_parent_id']);
-                $db->execute($sql, $params);
-
-                echo "done";
-            } catch (PDOException $e) {
-
-                echo "Lỗi khi thêm danh mục";
-                //echo  $sql . "<br>" . $e->getMessage();
-            }
-        }
-
-        function EditCategory($data){
-            try {
-                $db = new DB();
-                $sql = "UPDATE `Categories` SET `name` = ?, `parent_category_id` = ? WHERE `category_id` = ?;";
-                $params = array($data['category_name'], $data['category_parent_id'], $data['category_id']);
-                $db->execute($sql, $params);
-
-                echo "done";
-            } catch (PDOException $e) {
-
-                echo "Lỗi khi sửa danh mục";
-                //echo  $sql . "<br>" . $e->getMessage();
-            }
-        }
-
-        function DeleteCategory($data){
-            try {
-                $db = new DB();
-                $sql = "DELETE FROM `Categories` WHERE `category_id` = ?;";
-                $params = array($data['category_id']);
-                $db->execute($sql, $params);
-
-                echo "done";
-            } catch (PDOException $e) {
-
-                echo "Tồn tại sản phẩm thuộc danh mục này, không thể xóa";
-                //echo  $sql . "<br>" . $e->getMessage();
-            }
-        }
     }
 ?>
