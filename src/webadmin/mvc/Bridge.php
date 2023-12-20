98b4379bb671b64f725591da1a24a179a7e5c88d
Đạt Tấn <nguyentandat01102002@gmail.com>
2023-12-21 01:02:50 +0700

    csrf admin

--- src/webadmin/mvc/Bridge.php
+++ src/webadmin/mvc/Bridge.php
@@ -8,6 +8,13 @@
     ini_set('session.sid_bits_per_character', 6);
     //ini_set('session.sid_length_min', 64);
 
+    if(!in_array($requested_origin, $allowed_origins)) {
+        if(!empty($requested_origin) || ($_SERVER['HTTP_SEC_FETCH_SITE'] != "none" && ($_SERVER['HTTP_SEC_FETCH_SITE'] != "same-origin" ))){
+            require_once "./mvc/views/shopping/403.php";
+            exit;
+        }
+    }
+
     // Thiết lập độ dài Session ID tối đa là 128 ký tự  
     //ini_set('session.sid_length_max', 128);
     //ini_set('session.use_strict_mode', 0);
