ae7914fcfa8906014b4cddf181281e5956e198a7
thinc <caothiu2003@gmail.com>
2024-12-23 21:07:45 +0700

    finanl

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,13 @@ resource "azurerm_resource_group" "rg" {
   name     = "aks_rg"
 }
 
+resource "azurerm_role_assignment" "example" {
+  principal_id          = "24b215c2-b5ca-4272-a905-74a5acefe83a"  # Replace with the Object ID
+  role_definition_name  = "Contributor"          # Or "Reader", "Owner"
+  scope                 = azurerm_resource_group.rg.id
+}
+
+
 resource "random_pet" "azurerm_kubernetes_cluster_name" {
   prefix = "cluster"
 }
@@ -22,12 +29,7 @@ resource "azurerm_storage_account" "storage" {
   account_replication_type = "LRS"
 }
 
-# # Tạo Blob Container
-# resource "azurerm_storage_container" "blob" {
-#   name                  = "aks"
-#   storage_account_name  = azurerm_storage_account.storage.name
-#   container_access_type = "private"
-# }
+
 resource "azurerm_storage_share" "file_share" {
   name                 = "aks"
   storage_account_name = azurerm_storage_account.storage.name
@@ -62,32 +64,6 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   }
 }
 
-# provider "kubernetes" {
-#   host                   = "https://${azurerm_kubernetes_cluster.k8s.kube_config[0].host}"
-#   client_certificate     = base64decode(azurerm_kubernetes_cluster.k8s.kube_config[0].client_certificate)
-#   client_key             = base64decode(azurerm_kubernetes_cluster.k8s.kube_config[0].client_key)
-#   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.k8s.kube_config[0].cluster_ca_certificate)
-# }
-
-# resource "kubectl_manifest" "some_name" {
-#   yaml_body = <<YAML
-# apiVersion: storage.k8s.io/v1
-# kind: StorageClass
-# metadata:
-#   name: azure-blob-sc
-# provisioner: blob.csi.azure.com
-# reclaimPolicy: Retain
-# allowVolumeExpansion: true
-# YAML
-# }
-
-# Gán quyền truy cập cho AKS vào Azure Blob Storage
-# resource "azurerm_role_assignment" "aks_blob_access" {
-#   scope                = azurerm_storage_account.storage.id
-#   role_definition_name = "Storage Blob Data Contributor"
-#   principal_id         = azurerm_kubernetes_cluster.k8s.kubelet_identity[0].object_id
-# }
-
 resource "azurerm_container_registry" "acr" {
   name                = "N3TRegistry"
   resource_group_name = azurerm_resource_group.rg.name
@@ -110,8 +86,61 @@ resource "azurerm_mysql_flexible_server" "default" {
     iops    = 360
     size_gb = 20
   }
-  # provisioner "local-exec" {
-  #   command = "mysql -h ${self.fqdn} -u ${self.administrator_login} -p${self.administrator_password} < db.sql"
-  # }
+}
 
+resource "azurerm_storage_account" "account_synapse" {
+  name                     = "n3tworkstoragesynapse"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  is_hns_enabled           = "true"
+}
+
+resource "azurerm_storage_data_lake_gen2_filesystem" "lake" {
+  name               = "n3tworklake"
+  storage_account_id = azurerm_storage_account.account_synapse.id
+}
+
+resource "azurerm_synapse_workspace" "azurerm_synapse_workspace" {
+  name                                 = "n3tworksynapse"
+  resource_group_name                  = azurerm_resource_group.rg.name
+  location                             = azurerm_resource_group.rg.location
+  storage_data_lake_gen2_filesystem_id = azurerm_storage_data_lake_gen2_filesystem.lake.id
+  sql_administrator_login              = "sqladminuser"
+  sql_administrator_login_password     = "H@Sh1CoR3!"
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_app_service_plan" "funcapp" {
+  name                = "webshopAPI"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "FunctionApp"
+
+  sku {
+    tier     = "Dynamic"
+    size     = "Y1"  # "Y1" is the correct size for a consumption plan
+    capacity = 0
+  }
+}
+
+resource "azurerm_function_app" "funcapp" {
+  name                       = "webshopfunc"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  app_service_plan_id        = azurerm_app_service_plan.funcapp.id
+  storage_account_name       = azurerm_storage_account.storage.name
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+  os_type                    = "linux"  # Ensure the Function App runs on Linux
+  version                    = "~4"     # Use version "~4" for the latest supported runtime
+
+  app_settings = {
+    "FUNCTIONS_WORKER_RUNTIME" = "python"  # Specify Python as the worker runtime
+    "WEBSITE_RUN_FROM_PACKAGE" = "1"
+  }
 }
